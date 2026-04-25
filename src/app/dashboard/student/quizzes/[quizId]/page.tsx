b2d114d831ae40bb0e181c90a68e0edd70afe330
Saad <ihsaad.personal@gmail.com>
2026-04-25 23:27:40 +0600

    Relax UUID detection for Supabase seed IDs

--- src/app/dashboard/student/quizzes/[quizId]/page.tsx
+++ src/app/dashboard/student/quizzes/[quizId]/page.tsx
@@ -85,5 +85,5 @@ async function getSupabaseQuiz(quizId: string): Promise<Quiz | null> {
 }
 
 function isUuid(value: string) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(value);
 }
